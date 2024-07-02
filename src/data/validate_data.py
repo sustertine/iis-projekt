adb6a37d046108c2a7763c6785e7c16c15156665
Tine Šuster <tine.suster@gmai>
2024-07-02 18:46:36 +0200

    add gx

--- src/data/validate_data.py
+++ src/data/validate_data.py
@@ -1,6 +1,17 @@
+import great_expectations as ge
+from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
+
+
 def main():
-    print('validating data...')
+    context = ge.get_context()
+    result: CheckpointResult = context.run_checkpoint(checkpoint_name="air_quality")
+    if not result["success"]:
+        print(result)
+        print("Data validation failed")
+        return
+
+    print("Data validation passed!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
